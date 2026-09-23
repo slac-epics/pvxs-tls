ffb0a841068bc166cee2bb195c0d239bb88a50e7
george-mcintyre <george@level-n.com>
2026-09-23 16:23:46 +0200

    Keep Config::updateDefs virtual for downstream config subclasses

--- src/pvxs/server.h
+++ src/pvxs/server.h
@@ -214,7 +214,7 @@ public:
 
     //! extract definitions with environment variable names as keys.
     //! Process environment is not changed.
-    void updateDefs(defs_t& defs) const;
+    virtual void updateDefs(defs_t& defs) const;
 
     /** Apply rules to translate current requested configuration
      *  into one which can actually be loaded based on current host network configuration.
